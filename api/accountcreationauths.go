58d5092b49852bf99463c59595d02416f933fbb2
chainbing-dev <github@chainbing.io>
2021-08-17 17:55:38 +0800

    account update

---
+++ api/accountcreationauths.go
@@ -0,0 +1,86 @@
+package api
+
+import (
+	"errors"
+	"net/http"
+	"time"
+
+	ethCommon "github.com/ethereum/go-ethereum/common"
+	"github.com/gin-gonic/gin"
+	"github.com/chainbing/node/api/parsers"
+	"github.com/chainbing/node/common"
+	"github.com/chainbing/node/common/apitypes"
+	"github.com/iden3/go-iden3-crypto/babyjub"
+)
+
+func (a *API) postAccountCreationAuth(c *gin.Context) {
+	// Parse body
+	var apiAuth receivedAuth
+	if err := c.ShouldBindJSON(&apiAuth); err != nil {
+		retBadReq(&apiError{
+			Err:  err,
+			Code: ErrParamValidationFailedCode,
+			Type: ErrParamValidationFailedType}, c)
+		return
+	}
+	// API to common + verify signature
+	commonAuth := accountCreationAuthAPIToCommon(&apiAuth)
+	isValid, err := commonAuth.VerifySignature(a.cg.ChainID, a.chainbingAddress)
+	if !isValid && err != nil {
+		retBadReq(&apiError{
+			Err:  errors.New("invalid signature: " + err.Error()),
+			Code: ErrInvalidSignatureCode,
+			Type: ErrInvalidSignatureType,
+		}, c)
+		return
+	}
+	// Insert to DB
+	if err := a.l2.AddAccountCreationAuthAPI(commonAuth); err != nil {
+		retSQLErr(err, c)
+		return
+	}
+	type okResponse struct {
+		Success string `json:"success"`
+	}
+	// Return OK
+	c.JSON(http.StatusOK, &okResponse{
+		Success: "OK",
+	})
+}
+
+func (a *API) getAccountCreationAuth(c *gin.Context) {
+	// Get cbEthereumAddress
+	addr, err := parsers.ParseGetAccountCreationAuthFilter(c)
+	if err != nil {
+		retBadReq(&apiError{
+			Err:  err,
+			Code: ErrParamValidationFailedCode,
+			Type: ErrParamValidationFailedType,
+		}, c)
+		return
+	}
+	// Fetch auth from l2DB
+	auth, err := a.l2.GetAccountCreationAuthAPI(*addr)
+	if err != nil {
+		retSQLErr(err, c)
+		return
+	}
+	// Build successful response
+	c.JSON(http.StatusOK, auth)
+}
+
+type receivedAuth struct {
+	EthAddr   apitypes.StrCbEthAddr `json:"cbEthereumAddress" binding:"required"`
+	BJJ       apitypes.StrCbBJJ     `json:"bjj" binding:"required"`
+	Signature apitypes.EthSignature  `json:"signature" binding:"required"`
+	Timestamp time.Time              `json:"timestamp"`
+}
+
+func accountCreationAuthAPIToCommon(apiAuth *receivedAuth) *common.AccountCreationAuth {
+	return &common.AccountCreationAuth{
+		EthAddr:   ethCommon.Address(apiAuth.EthAddr),
+		BJJ:       (babyjub.PublicKeyComp)(apiAuth.BJJ),
+		Signature: []byte(apiAuth.Signature),
+		Timestamp: apiAuth.Timestamp,
+	}
+}
---
+++ common/accountcreationauths.go
@@ -0,0 +1,168 @@
+package common
+
+import (
+	"encoding/hex"
+	"fmt"
+	"time"
+
+	ethCommon "github.com/ethereum/go-ethereum/common"
+	ethMath "github.com/ethereum/go-ethereum/common/math"
+	ethCrypto "github.com/ethereum/go-ethereum/crypto"
+	ethSigner "github.com/ethereum/go-ethereum/signer/core"
+	"github.com/chainbing/tracerr"
+	"github.com/iden3/go-iden3-crypto/babyjub"
+)
+
+const (
+	// AccountCreationAuthMsg is the message that is signed to authorize a
+	// Chainbing account creation
+	AccountCreationAuthMsg = "Account creation"
+	// EIP712Version is the used version of the EIP-712
+	EIP712Version = "1"
+	// EIP712Provider defines the Provider for the EIP-712
+	EIP712Provider = "Chainbing Network"
+)
+
+var (
+	// EmptyEthSignature is an ethereum signature of all zeroes
+	EmptyEthSignature = make([]byte, 65)
+)
+
+// AccountCreationAuth authorizations sent by users to the L2DB, to be used for
+// account creations when necessary
+type AccountCreationAuth struct {
+	EthAddr   ethCommon.Address     `meddler:"eth_addr"`
+	BJJ       babyjub.PublicKeyComp `meddler:"bjj"`
+	Signature []byte                `meddler:"signature"`
+	Timestamp time.Time             `meddler:"timestamp,utctime"`
+}
+
+// toHash returns a byte array to be hashed from the AccountCreationAuth, which
+// follows the EIP-712 encoding
+func (a *AccountCreationAuth) toHash(chainID uint16,
+	chainbingContractAddr ethCommon.Address) ([]byte, error) {
+	chainIDFormatted := ethMath.NewHexOrDecimal256(int64(chainID))
+
+	signerData := ethSigner.TypedData{
+		Types: ethSigner.Types{
+			"EIP712Domain": []ethSigner.Type{
+				{Name: "name", Type: "string"},
+				{Name: "version", Type: "string"},
+				{Name: "chainId", Type: "uint256"},
+				{Name: "verifyingContract", Type: "address"},
+			},
+			"Authorise": []ethSigner.Type{
+				{Name: "Provider", Type: "string"},
+				{Name: "Authorisation", Type: "string"},
+				{Name: "BJJKey", Type: "bytes32"},
+			},
+		},
+		PrimaryType: "Authorise",
+		Domain: ethSigner.TypedDataDomain{
+			Name:              EIP712Provider,
+			Version:           EIP712Version,
+			ChainId:           chainIDFormatted,
+			VerifyingContract: chainbingContractAddr.Hex(),
+		},
+		Message: ethSigner.TypedDataMessage{
+			"Provider":      EIP712Provider,
+			"Authorisation": AccountCreationAuthMsg,
+			"BJJKey":        SwapEndianness(a.BJJ[:]),
+		},
+	}
+
+	domainSeparator, err := signerData.HashStruct("EIP712Domain", signerData.Domain.Map())
+	if err != nil {
+		return nil, tracerr.Wrap(err)
+	}
+	typedDataHash, err := signerData.HashStruct(signerData.PrimaryType, signerData.Message)
+	if err != nil {
+		return nil, tracerr.Wrap(err)
+	}
+
+	rawData := []byte{0x19, 0x01} // "\x19\x01"
+	rawData = append(rawData, domainSeparator...)
+	rawData = append(rawData, typedDataHash...)
+	return rawData, nil
+}
+
+// HashToSign returns the hash to be signed by the Ethereum address to authorize
+// the account creation, which follows the EIP-712 encoding
+func (a *AccountCreationAuth) HashToSign(chainID uint16,
+	chainbingContractAddr ethCommon.Address) ([]byte, error) {
+	b, err := a.toHash(chainID, chainbingContractAddr)
+	if err != nil {
+		return nil, tracerr.Wrap(err)
+	}
+	return ethCrypto.Keccak256(b), nil
+}
+
+// Sign signs the account creation authorization message using the provided
+// `signHash` function, and stores the signature in `a.Signature`.  `signHash`
+// should do an ethereum signature using the account corresponding to
+// `a.EthAddr`.  The `signHash` function is used to make signing flexible: in
+// tests we sign directly using the private key, outside tests we sign using
+// the keystore (which never exposes the private key). Sign follows the EIP-712
+// encoding.
+func (a *AccountCreationAuth) Sign(signHash func(hash []byte) ([]byte, error),
+	chainID uint16, chainbingContractAddr ethCommon.Address) error {
+	hash, err := a.HashToSign(chainID, chainbingContractAddr)
+	if err != nil {
+		return tracerr.Wrap(err)
+	}
+	sig, err := signHash(hash)
+	if err != nil {
+		return tracerr.Wrap(err)
+	}
+	sig[64] += 27
+	a.Signature = sig
+	a.Timestamp = time.Now()
+	return nil
+}
+
+// VerifySignature ensures that the Signature is done with the EthAddr, for the
+// chainID and chainbingContractAddress passed by parameter. VerifySignature
+// follows the EIP-712 encoding.
+func (a *AccountCreationAuth) VerifySignature(chainID uint16,
+	chainbingContractAddr ethCommon.Address) (bool, error) {
+	// Calculate hash to be signed
+	hash, err := a.HashToSign(chainID, chainbingContractAddr)
+	if err != nil {
+		signatureV := hex.EncodeToString(a.Signature)
+		return false, fmt.Errorf("error calculating hash to be signed: %s. "+
+			"ChainId: %d. ChainbingContractAddress: %s. EthereumAddress: %s. Bjj: %s. "+
+			"Signature: %s. Timestamp: %s", err.Error(), chainID, chainbingContractAddr.String(),
+			a.EthAddr.String(), BjjToString(a.BJJ), signatureV, a.Timestamp.String())
+	}
+
+	var sig [65]byte
+	copy(sig[:], a.Signature[:])
+	sig[64] -= 27
+
+	// Get public key from Signature
+	pubKBytes, err := ethCrypto.Ecrecover(hash, sig[:])
+	if err != nil {
+		signatureV := hex.EncodeToString(a.Signature)
+		return false, fmt.Errorf("error getting public key from Signature: %s. "+
+			"ChainId: %d. ChainbingContractAddress: %s. EthereumAddress: %s. Bjj: %s. "+
+			"Signature: %s. Timestamp: %s", err.Error(), chainID, chainbingContractAddr.String(),
+			a.EthAddr.String(), BjjToString(a.BJJ), signatureV, a.Timestamp.String())
+	}
+	pubK, err := ethCrypto.UnmarshalPubkey(pubKBytes)
+	if err != nil {
+		signatureV := hex.EncodeToString(a.Signature)
+		return false, fmt.Errorf("error unmarshalling public key: %s. ChainId: %d. "+
+			"ChainbingContractAddress: %s. EthereumAddress: %s. Bjj: %s. Signature: %s. "+
+			"Timestamp: %s", err.Error(), chainID, chainbingContractAddr.String(),
+			a.EthAddr.String(), BjjToString(a.BJJ), signatureV, a.Timestamp.String())
+	}
+	// Get addr from pubK
+	addr := ethCrypto.PubkeyToAddress(*pubK)
+	if addr != a.EthAddr {
+		signatureV := hex.EncodeToString(a.Signature)
+		return false, fmt.Errorf("error: Ethereum address doesn't match with the one used in the signature. "+
+			"ChainId: %d. ChainbingContractAddress: %s. EthereumAddress: %s. Bjj: %s. Signature: %s. Timestamp: %s",
+			chainID, chainbingContractAddr.String(), a.EthAddr.String(), BjjToString(a.BJJ), signatureV, a.Timestamp.String())
+	}
+	return true, nil
+}
