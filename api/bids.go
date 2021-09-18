e38217a788480843739e50de673d8764aa3d3525
chainbing-dev <github@chainbing.io>
2021-09-18 17:02:49 +0800

    bids

---
+++ api/bids.go
@@ -0,0 +1,44 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/chainbing/node/api/parsers"
+	"github.com/chainbing/node/db/historydb"
+)
+
+func (a *API) getBids(c *gin.Context) {
+	filters, err := parsers.ParseBidsFilters(c, a.validate)
+	if err != nil {
+		retBadReq(&apiError{
+			Err:  err,
+			Code: ErrParamValidationFailedCode,
+			Type: ErrParamValidationFailedType,
+		}, c)
+		return
+	}
+
+	bids, pendingItems, err := a.h.GetBidsAPI(historydb.GetBidsAPIRequest{
+		SlotNum:    filters.SlotNum,
+		BidderAddr: filters.BidderAddr,
+		FromItem:   filters.FromItem,
+		Limit:      filters.Limit,
+		Order:      filters.Order,
+	})
+
+	if err != nil {
+		retSQLErr(err, c)
+		return
+	}
+
+	// Build successful response
+	type bidsResponse struct {
+		Bids         []historydb.BidAPI `json:"bids"`
+		PendingItems uint64             `json:"pendingItems"`
+	}
+	c.JSON(http.StatusOK, &bidsResponse{
+		Bids:         bids,
+		PendingItems: pendingItems,
+	})
+}
---
+++ batchbuilder/batchbuilder_test.go
@@ -0,0 +1,42 @@
+package batchbuilder
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/chainbing/node/db/statedb"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var deleteme []string
+
+func TestMain(m *testing.M) {
+	exitVal := m.Run()
+	for _, dir := range deleteme {
+		if err := os.RemoveAll(dir); err != nil {
+			panic(err)
+		}
+	}
+	os.Exit(exitVal)
+}
+
+func TestBatchBuilder(t *testing.T) {
+	dir, err := ioutil.TempDir("", "tmpdb")
+	require.Nil(t, err)
+	deleteme = append(deleteme, dir)
+
+	synchDB, err := statedb.NewStateDB(statedb.Config{Path: dir, Keep: 128,
+		Type: statedb.TypeBatchBuilder, NLevels: 0})
+	assert.Nil(t, err)
+
+	bbDir, err := ioutil.TempDir("", "tmpBatchBuilderDB")
+	require.Nil(t, err)
+	deleteme = append(deleteme, bbDir)
+	bb, err := NewBatchBuilder(bbDir, synchDB, 0, 32)
+	assert.Nil(t, err)
+
+	bb.LocalStateDB().Close()
+	synchDB.Close()
+}
