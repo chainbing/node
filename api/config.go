4eb2c12d1d72e3e4912251e40d250c06cf7a4970
chainbing-dev <github@chainbing.io>
2021-10-09 11:10:58 +0800

    config

---
+++ api/config.go
@@ -0,0 +1,67 @@
+package api
+
+import (
+	"math/big"
+	"net/http"
+
+	ethCommon "github.com/ethereum/go-ethereum/common"
+	"github.com/gin-gonic/gin"
+	"github.com/chainbing/node/common"
+)
+
+type rollupConstants struct {
+	PublicConstants         common.RollupConstants `json:"publicConstants"`
+	MaxFeeIdxCoordinator    int                    `json:"maxFeeIdxCoordinator"`
+	ReservedIdx             int                    `json:"reservedIdx"`
+	ExitIdx                 int                    `json:"exitIdx"`
+	LimitDepositAmount      *big.Int               `json:"limitDepositAmount"`
+	LimitL2TransferAmount   *big.Int               `json:"limitL2TransferAmount"`
+	LimitTokens             int                    `json:"limitTokens"`
+	L1CoordinatorTotalBytes int                    `json:"l1CoordinatorTotalBytes"`
+	L1UserTotalBytes        int                    `json:"l1UserTotalBytes"`
+	MaxL1UserTx             int                    `json:"maxL1UserTx"`
+	MaxL1Tx                 int                    `json:"maxL1Tx"`
+	InputSHAConstantBytes   int                    `json:"inputSHAConstantBytes"`
+	NumBuckets              int                    `json:"numBuckets"`
+	MaxWithdrawalDelay      int                    `json:"maxWithdrawalDelay"`
+	ExchangeMultiplier      int                    `json:"exchangeMultiplier"`
+}
+
+func newRollupConstants(publicConstants common.RollupConstants) *rollupConstants {
+	return &rollupConstants{
+		PublicConstants:         publicConstants,
+		MaxFeeIdxCoordinator:    common.RollupConstMaxFeeIdxCoordinator,
+		ReservedIdx:             common.RollupConstReservedIDx,
+		ExitIdx:                 common.RollupConstExitIDx,
+		LimitDepositAmount:      common.RollupConstLimitDepositAmount,
+		LimitL2TransferAmount:   common.RollupConstLimitL2TransferAmount,
+		LimitTokens:             common.RollupConstLimitTokens,
+		L1CoordinatorTotalBytes: common.RollupConstL1CoordinatorTotalBytes,
+		L1UserTotalBytes:        common.RollupConstL1UserTotalBytes,
+		MaxL1UserTx:             common.RollupConstMaxL1UserTx,
+		MaxL1Tx:                 common.RollupConstMaxL1Tx,
+		InputSHAConstantBytes:   common.RollupConstInputSHAConstantBytes,
+		MaxWithdrawalDelay:      common.RollupConstMaxWithdrawalDelay,
+		ExchangeMultiplier:      common.RollupConstExchangeMultiplier,
+	}
+}
+
+// Config of the API
+type Config struct {
+	RollupConstants   common.RollupConstants
+	AuctionConstants  common.AuctionConstants
+	WDelayerConstants common.WDelayerConstants
+	ChainID           uint16
+	ChainbingAddress     ethCommon.Address
+}
+
+type configAPI struct {
+	ChainID           uint16                   `json:"chainId"`
+	RollupConstants   rollupConstants          `json:"chainbing"`
+	AuctionConstants  common.AuctionConstants  `json:"auction"`
+	WDelayerConstants common.WDelayerConstants `json:"withdrawalDelayer"`
+}
+
+func (a *API) getConfig(c *gin.Context) {
+	c.JSON(http.StatusOK, a.cg)
+}
---
+++ api/config_test.go
@@ -0,0 +1,55 @@
+package api
+
+import (
+	"math/big"
+	"testing"
+
+	ethCommon "github.com/ethereum/go-ethereum/common"
+	"github.com/chainbing/node/common"
+	"github.com/stretchr/testify/assert"
+)
+
+func getConfigTest(chainID uint16) Config {
+	var config Config
+
+	var rollupPublicConstants common.RollupConstants
+	rollupPublicConstants.AbsoluteMaxL1L2BatchTimeout = 240
+	rollupPublicConstants.ChainbingAuctionContract = ethCommon.HexToAddress("0x500D1d6A4c7D8Ae28240b47c8FCde034D827fD5e")
+	rollupPublicConstants.ChainbingGovernanceAddress = ethCommon.HexToAddress("0xeAD9C93b79Ae7C1591b1FB5323BD777E86e150d4")
+	rollupPublicConstants.TokenCB = ethCommon.HexToAddress("0xf784709d2317D872237C4bC22f867d1BAe2913AB")
+	rollupPublicConstants.WithdrawDelayerContract = ethCommon.HexToAddress("0xD6C850aeBFDC46D7F4c207e445cC0d6B0919BDBe")
+	var verifier common.RollupVerifierStruct
+	verifier.MaxTx = 512
+	verifier.NLevels = 32
+	rollupPublicConstants.Verifiers = append(rollupPublicConstants.Verifiers, verifier)
+
+	var auctionConstants common.AuctionConstants
+	auctionConstants.BlocksPerSlot = 40
+	auctionConstants.GenesisBlockNum = 100
+	auctionConstants.GovernanceAddress = ethCommon.HexToAddress("0xeAD9C93b79Ae7C1591b1FB5323BD777E86e150d4")
+	auctionConstants.InitialMinimalBidding, _ = new(big.Int).SetString("10000000000000000000", 10)
+	auctionConstants.ChainbingRollup = ethCommon.HexToAddress("0xEa960515F8b4C237730F028cBAcF0a28E7F45dE0")
+	auctionConstants.TokenCB = ethCommon.HexToAddress("0xf784709d2317D872237C4bC22f867d1BAe2913AB")
+
+	var wdelayerConstants common.WDelayerConstants
+	wdelayerConstants.ChainbingRollup = ethCommon.HexToAddress("0xEa960515F8b4C237730F028cBAcF0a28E7F45dE0")
+	wdelayerConstants.MaxEmergencyModeTime = uint64(1000000)
+	wdelayerConstants.MaxWithdrawalDelay = uint64(10000000)
+
+	config.RollupConstants = rollupPublicConstants
+	config.AuctionConstants = auctionConstants
+	config.WDelayerConstants = wdelayerConstants
+
+	config.ChainID = chainID
+	config.ChainbingAddress = ethCommon.HexToAddress("0xc344E203a046Da13b0B4467EB7B3629D0C99F6E6")
+
+	return config
+}
+
+func TestGetConfig(t *testing.T) {
+	endpoint := apiURL + "config"
+	var configTest configAPI
+	assert.NoError(t, doGoodReq("GET", endpoint, nil, &configTest))
+	assert.Equal(t, config, configTest)
+	assert.Equal(t, api.cg, &configTest)
+}
