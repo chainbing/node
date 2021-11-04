6fa9e8b0577d45eb8761bf047623b698d6db7ac2
chainbing-dev <github@chainbing.io>
2021-11-04 10:37:20 +0800

    health

---
+++ api/health.go
@@ -0,0 +1,42 @@
+package api
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/dimiro1/health"
+	ethCommon "github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/chainbing/node/health/checkers"
+)
+
+func (a *API) healthRoute(version string, ethClient *ethclient.Client, forgerAddress *ethCommon.Address) http.Handler {
+	// taking two checkers for one db in case that in
+	// the future there will be two separated dbs
+	healthHandler := health.NewHandler()
+
+	if a.l2 != nil {
+		l2DBChecker := checkers.NewCheckerWithDB(a.l2.DB().DB)
+		healthHandler.AddChecker("l2DB", l2DBChecker)
+	}
+	if a.h != nil {
+		historyDBChecker := checkers.NewCheckerWithDB(a.h.DB().DB)
+		healthHandler.AddChecker("historyDB", historyDBChecker)
+	}
+	if a.stateDB != nil {
+		stateDBChecker := checkers.NewStateDBChecker(a.stateDB)
+		healthHandler.AddChecker("stateDB", stateDBChecker)
+	}
+	healthHandler.AddInfo("version", version)
+	t := time.Now().UTC()
+	healthHandler.AddInfo("timestamp", t)
+	if ethClient != nil && forgerAddress != nil {
+		balance, err := ethClient.BalanceAt(context.TODO(), *forgerAddress, nil)
+		if err != nil {
+			return healthHandler
+		}
+		healthHandler.AddInfo("coordinatorForgerBalance", balance.String())
+	}
+	return healthHandler
+}
